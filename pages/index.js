81a2a6fac109d68d8d6b22f6656b58234c61eda7
thedoft <thedoft@gmail.com>
2020-08-05 00:03:23 +0300

    feat: index.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,34 @@
-let popup = document.querySelector('.popup');
-let editButton = document.querySelector('.profile__edit-button');
-let closeButton = popup.querySelector('.popup__close-button')
+let profile = document.querySelector('.profile');
+let profileName = profile.querySelector('.profile__title');
+let profileJob = profile.querySelector('.profile__subtitle');
+let editButton = profile.querySelector('.profile__edit-button');
 
-function togglePopupOpened() {
-  popup.classList.toggle('popup_opened');
+let popup = document.querySelector('.popup');
+let form = popup.querySelector('.popup__container');
+let closeButton = popup.querySelector('.popup__close-button');
+let inputs = popup.querySelectorAll('.popup__input');
+
+function openPopup() {
+  popup.classList.add('popup_opened');
+
+  inputs[0].value = profileName.textContent;
+  inputs[1].value = profileJob.textContent;
 }
 
-editButton.addEventListener('click', togglePopupOpened);
-closeButton.addEventListener('click', togglePopupOpened);
+function closePopup() {
+  popup.classList.remove('popup_opened');
+}
+
+editButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
+
+function saveProfile (evt) {
+  evt.preventDefault();
+
+  profileName.textContent = inputs[0].value;
+  profileJob.textContent = inputs[1].value;
+
+  closePopup();
+}
+
+form.addEventListener('submit', saveProfile);
