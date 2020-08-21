9dc0be9a1257fbc416f139b5c61c3c27e0c2dfbc
thedoft <thedoft@gmail.com>
2020-08-21 21:18:56 +0300

    feat: addEventListener for buttons

--- scripts/index.js
+++ scripts/index.js
@@ -26,7 +26,7 @@ const initialCards = [
   }
 ];
 
-// выбор списка карточек и заготовки их верстки
+// выбор списка карточек, самой карточки и заготовки ее верстки
 const elementsList = document.querySelector('.elements__list');
 const elementTemplate = document.querySelector('#element').content;
 
@@ -38,6 +38,13 @@ initialCards.forEach(item => {
   element.querySelector('.element__image').alt = item.name;
   element.querySelector('.element__title').textContent = item.name;
 
+  // выбор кнопки like, функция, делающая ее активной/неактивной при нажатии и добавление функции кнопке
+  const likeButton = element.querySelector('.element__like-button');
+
+  likeButton.addEventListener('click', function(evt) {
+    evt.target.classList.toggle('element__like-button_active');
+  });
+
   elementsList.append(element);
 });
 
@@ -48,41 +55,71 @@ const profileJob = profile.querySelector('.profile__subtitle');
 const editButton = profile.querySelector('.profile__edit-button');
 const addButton = profile.querySelector('.profile__add-button');
 
-//выбор элементов блока popup
-const popup = document.querySelector('.popup');
-const form = popup.querySelector('.popup__container');
-const closeButton = popup.querySelector('.popup__close-button');
-const inputName = popup.querySelector('.popup__input_type_name');
-const inputJob = popup.querySelector('.popup__input_type_job');
-const inputPlace = popup.querySelector('popup__input_type_place');
-const inputLink = popup.querySelector('popup__input_type_link');
+// выбор заготовки верстки попапа, создание двух попапов с формой и добавление их в DOM
+const page = document.querySelector('.page');
+const popupTemplate = document.querySelector('#popup').content;
 
-// функция открытия попапа
-function openPopup() {
-  popup.classList.add('popup_opened');
+const editPopup = popupTemplate.cloneNode(true);
+editPopup.querySelector('.popup__title').textContent = 'Редактирование профиля';
+editPopup.querySelector('.popup__save-button').textContent = 'Сохранить';
 
-  inputName.value = profileName.textContent;
-  inputJob.value = profileJob.textContent;
-}
+const addPopup = popupTemplate.cloneNode(true);
+addPopup.querySelector('.popup__title').textContent = 'Новое место';
+addPopup.querySelector('.popup__save-button').textContent = 'Создать';
+addPopup.querySelectorAll('.popup__input')[0].placeholder = 'Название';
+addPopup.querySelectorAll('.popup__input')[1].placeholder = 'Ссылка на картинку';
+
+page.after(editPopup, addPopup);
+const popup = document.querySelectorAll('.popup');
+
+// функции открытия попапа
+function openEditPopup() {
+  popup[0].classList.add('popup_opened');
+
+  popup[0].querySelectorAll('.popup__input')[0].value = profileName.textContent;
+  popup[0].querySelectorAll('.popup__input')[1].value = profileJob.textContent;
+};
+
+function openAddPopup() {
+  popup[1].classList.add('popup_opened');
+};
 
 // функция закрытия попапа
 function closePopup() {
-  popup.classList.remove('popup_opened');
-}
+  popup.forEach(item => {
+    item.classList.remove('popup_opened')});
+};
 
-// добавление слушателя событий на кнопки редактирования профиля и закрытия попапа
-editButton.addEventListener('click', openPopup);
-closeButton.addEventListener('click', closePopup);
+// добавление слушателя событий на кнопки редактирования профиля, добавления карточки и закрытия попапа
+editButton.addEventListener('click', openEditPopup);
+addButton.addEventListener('click', openAddPopup);
+
+const closeButton = document.querySelectorAll('.popup__close-button');
+closeButton.forEach(item => {
+  item.addEventListener('click', closePopup)});
+
+// выбор форм
+const form = document.querySelectorAll('.popup__container');
 
 // функция-обработчик события клика на кнопку попапа Сохранить
 function saveProfile (evt) {
   evt.preventDefault();
 
-  profileName.textContent = inputName.value;
-  profileJob.textContent = inputJob.value;
+  profileName.textContent = popup[0].querySelectorAll('.popup__input')[0].value;
+  profileJob.textContent = popup[0].querySelectorAll('.popup__input')[1].value;
+
   closePopup();
-}
+};
 
-// добавление слушателя событий на форму
-form.addEventListener('submit', saveProfile);
+// функция-обработчик события клика на кнопку попапа Создать
+function addCard (evt) {
+  evt.preventDefault();
 
+
+
+  closePopup();
+};
+
+// добавление слушателей событий на формы
+form[0].addEventListener('submit', saveProfile);
+form[1].addEventListener('submit', addCard);
