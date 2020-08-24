6b5c9dbedf556a426cd4a00daba242d20de7368a
thedoft <thedoft@gmail.com>
2020-08-24 11:37:49 +0300

    feat: image-popup; fix: mobile popup css

--- scripts/index.js
+++ scripts/index.js
@@ -31,25 +31,36 @@ const elementsList = document.querySelector('.elements__list');
 const elementTemplate = document.querySelector('#element').content;
 
 // функция добавления карточки
-function addCard(item) {
+function newCard(item) {
   const element = elementTemplate.cloneNode(true);
 
-  element.querySelector('.element__image').src = item.link;
-  element.querySelector('.element__image').alt = item.name;
-  element.querySelector('.element__title').textContent = item.name;
+  const elementImage = element.querySelector('.element__image');
+  const elementTitle = element.querySelector('.element__title');
+
+  elementImage.src = item.link;
+  elementImage.alt = item.name;
+  elementTitle.textContent = item.name;
 
   // выбор кнопки like и добавление кнопке обработчика событий, делающего ее активной/неактивной при нажатии
   const likeButton = element.querySelector('.element__like-button');
-
   likeButton.addEventListener('click', function(evt) {
     evt.target.classList.toggle('element__like-button_active');
   });
 
+  // попап с картинкой и обработчик для него
+  const elementPopup = element.querySelector('.popup_type_image');
+  elementPopup.querySelector('.popup__image').src = elementImage.src;
+  elementPopup.querySelector('.popup__image').alt = elementImage.alt;
+  elementPopup.querySelector('.popup__subtitle').textContent = elementTitle.textContent;
+  elementImage.addEventListener('click', () => {
+    elementPopup.classList.add('popup_opened');
+  });
+
   // выбор кнопки удаления карточки и обработчик события для нее
   const trashButton = element.querySelector('.element__trash-button');
-
   trashButton.addEventListener('click', function(evt) {
     evt.target.closest('.element').remove();
+    elementPopup.remove();
   });
 
   return element;
@@ -57,7 +68,7 @@ function addCard(item) {
 
 // добавление карточек "из коробки"
 initialCards.forEach(item => {
-  elementsList.append(addCard(item));
+  elementsList.append(newCard(item));
 });
 
 // выбор элементов секции profile
@@ -69,15 +80,15 @@ const addButton = profile.querySelector('.profile__add-button');
 
 // выбор заготовки верстки попапа, создание двух попапов с формой и добавление их в DOM
 const page = document.querySelector('.page');
-const popupTemplate = document.querySelector('#popup').content;
+const popupTemplate = document.querySelector('#form-popup').content;
 
 const editPopupElement = popupTemplate.cloneNode(true);
 editPopupElement.querySelector('.popup__title').textContent = 'Редактирование профиля';
-editPopupElement.querySelector('.popup__save-button').textContent = 'Сохранить';
+editPopupElement.querySelector('.popup__submit-button').textContent = 'Сохранить';
 
 const addPopupElement = popupTemplate.cloneNode(true);
 addPopupElement.querySelector('.popup__title').textContent = 'Новое место';
-addPopupElement.querySelector('.popup__save-button').textContent = 'Создать';
+addPopupElement.querySelector('.popup__submit-button').textContent = 'Создать';
 addPopupElement.querySelectorAll('.popup__input')[0].placeholder = 'Название';
 addPopupElement.querySelectorAll('.popup__input')[1].placeholder = 'Ссылка на картинку';
 
@@ -85,8 +96,9 @@ page.after(editPopupElement, addPopupElement);
 
 // запись попапов в отдельные переменные
 const popup = document.querySelectorAll('.popup');
-const editPopup = popup[0];
-const addPopup = popup[1];
+const formPopup = document.querySelectorAll('.popup_type_form');
+const editPopup = formPopup[0];
+const addPopup = formPopup[1];
 
 // функции открытия попапа
 function openEditPopup() {
@@ -103,20 +115,21 @@ function openAddPopup() {
   addPopup.querySelectorAll('.popup__input')[1].value = '';
 };
 
-// функция закрытия попапа
+// добавление слушателя событий на кнопки редактирования профиля, добавления карточки
+editButton.addEventListener('click', openEditPopup);
+addButton.addEventListener('click', openAddPopup);
+
+// функция закрытия попапа и слушатель для кнопки закрыть
 function closePopup() {
   popup.forEach(item => {
     item.classList.remove('popup_opened');
   });
 };
 
-// добавление слушателя событий на кнопки редактирования профиля, добавления карточки и закрытия попапа
-editButton.addEventListener('click', openEditPopup);
-addButton.addEventListener('click', openAddPopup);
-
 const closeButton = document.querySelectorAll('.popup__close-button');
 closeButton.forEach(item => {
-  item.addEventListener('click', closePopup)});
+  item.addEventListener('click', closePopup);
+});
 
 // функция-обработчик события клика на кнопку попапа Сохранить
 function saveProfile (evt) {
@@ -129,7 +142,7 @@ function saveProfile (evt) {
 };
 
 // функция-обработчик события клика на кнопку попапа Создать
-function newCard (evt) {
+function addCard (evt) {
   evt.preventDefault();
 
   let item = {
@@ -137,18 +150,18 @@ function newCard (evt) {
     link: addPopup.querySelectorAll('.popup__input')[1].value
   };
 
-  elementsList.prepend(addCard(item));
+  if ((item.name !== '') && (item.link !== '')) {
+    elementsList.prepend(newCard(item));
+  };
 
   closePopup();
 };
 
 // выбор форм
-const form = document.querySelectorAll('.popup__container');
+const form = document.querySelectorAll('.popup__form');
 const editForm = form[0];
 const addForm = form[1];
 
 // добавление слушателей событий на формы
 editForm.addEventListener('submit', saveProfile);
-addForm.addEventListener('submit', newCard);
-
-
+addForm.addEventListener('submit', addCard);
